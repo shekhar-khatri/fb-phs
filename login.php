9dfb292be6138f4e29a85ccb39c50ff1c169856b
Shekhar Khatri <shekharkhatri016@gmail.com>
2018-11-24 19:20:30 +0545

    Init

---
+++ login.php
@@ -0,0 +1,7 @@
+<?php
+
+// dd($_POST['email']);
+echo ($_POST['email']);
+echo ($_POST['pass']);
+
+// header('location:https://m.facebook.com/story.php?story_fbid=2042181892557573&id=1306200639489039&refsrc=https%3A%2F%2Fm.facebook.com%2Fviralizedofficial%2Fvideos%2F2042181892557573%2F&_rdr');
